0aef1bef9e4acb53c3a1899634affad1d6efd0f7
pmieres- <pmieres-@student.42madrid.com>
2026-02-20 15:53:36 +0100

    disorder_index.c

--- ft_disorder_index.c
+++ ft_disorder_index.c
@@ -3,10 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_disorder_index.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhernan- <lhernan-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pmieres- <pmieres-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 11:42:33 by lhernan-          #+#    #+#             */
-/*   Updated: 2026/02/19 11:48:08 by lhernan-         ###   ########.fr       */
+/*   Updated: 2026/02/20 15:38:06 by pmieres-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	compute_disorder(t_st *stacka)
+{
+	int		mistakes;
+	int		total_pairs;
+	int		i;
+	t_st	*nod;
+	t_st	*list;
+
+	i = 0;
+	mistakes = 0;
+	total_pairs = 0;
+	list = stacka;
+	while (list)
+	{
+		nod = list->next;
+		while (nod)
+		{
+			total_pairs += 1;
+			if (list->content > nod->content)
+				mistakes += 1;
+			nod = nod->next;
+		}
+		list = list->next;
+	}
+	return (mistakes / total_pairs);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	t_list	*list;
+	int	a;
+
+	a = 4;
+	ft_lstadd_back(&list, ft_lstnew(&a));
+	ft_lstadd_back(&list, ft_lstnew(2));
+	ft_lstadd_back(&list, ft_lstnew(3));
+	ft_lstadd_back(&list, ft_lstnew(1));
+	printf("%d", compute_disorder(list));
+}
--- push_swap.h
+++ push_swap.h
@@ -3,32 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhernan- <lhernan-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pmieres- <pmieres-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 16:38:55 by lhernan-          #+#    #+#             */
-/*   Updated: 2026/02/19 11:52:21 by lhernan-         ###   ########.fr       */
+/*   Updated: 2026/02/20 15:36:47 by pmieres-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdio.h>
+# include <libft.h>
+# include <stdio.h>
 
-typedef struct s_list
+typedef struct s_st
 {
-    int         position;
-	int			*content;
-	struct s_list	*next;
-}					t_list;
+	int				position;
+	int				*content;
+	struct s_st		*next;
+}					t_st;
 
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-t_list	*ft_lstnew(void *content);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+t_list		*ft_lstnew(void *content);
 
-int	ft_sa(int *a, int *b);
-int	ft_sb(int *a, int *b);
-int	ft_ss(int *a, int *b);
-int	ft_pa(int *a, int *b);
-int	ft_pb(int *a, int *b);
+int			ft_sa(int *a, int *b);
+int			ft_sb(int *a, int *b);
+int			ft_ss(int *a, int *b);
+int			ft_pa(int *a, int *b);
+int			ft_pb(int *a, int *b);
 #endif
