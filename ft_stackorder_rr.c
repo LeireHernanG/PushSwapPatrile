cef8cf942c7033cf23bbcec3d5a79fd2db48c9fe
pmieres- <pmieres-@student.42madrid.com>
2026-02-24 18:44:57 +0100

    He puesto a nulo el final de cada lista en los movimientos de r

--- ft_stackorder_rr.c
+++ ft_stackorder_rr.c
@@ -6,52 +6,56 @@
 /*   By: pmieres- <pmieres-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 11:41:51 by lhernan-          #+#    #+#             */
-/*   Updated: 2026/02/24 13:02:57 by pmieres-         ###   ########.fr       */
+/*   Updated: 2026/02/24 18:44:07 by pmieres-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int ft_rra(t_stack **a)
+int	ft_rra(t_stack **a)
 {
-    t_stack *lastposition;
-    t_stack *new;
-    if(!a || !*a)
-        return(0);
-    lastposition = *a;
-    while (lastposition->next)
-    {
-        new = lastposition;
-        lastposition = lastposition->next;
-    }
-    lastposition -> next = *a;
-    *a = lastposition;
-    //ft_printf("%s","rra");
-    return (1);
+	t_stack	*lastposition;
+	t_stack	*new;
+
+	if (!a || !*a)
+		return (0);
+	lastposition = *a;
+	while (lastposition->next)
+	{
+		new = lastposition;
+		lastposition = lastposition->next;
+	}
+	lastposition->next = *a;
+	*a = lastposition;
+	new->next = NULL;
+	// ft_printf("%s","rra");
+	return (1);
 }
 
-int ft_rrb(t_stack **b)
+int	ft_rrb(t_stack **b)
 {
-    t_stack *lastposition;
-    t_stack *new;
-    if(!b || !*b)
-        return(0);
-    lastposition = *b;
-    while (lastposition->next)
-    {
-        new = lastposition;
-        lastposition = lastposition->next;
-    }
-    lastposition -> next = *b;
-    *b = lastposition;
-    //ft_printf("%s","rrb");
-    return (1);
+	t_stack	*lastposition;
+	t_stack	*new;
+
+	if (!b || !*b)
+		return (0);
+	lastposition = *b;
+	while (lastposition->next)
+	{
+		new = lastposition;
+		lastposition = lastposition->next;
+	}
+	lastposition->next = *b;
+	*b = lastposition;
+	new->next = NULL;
+	// ft_printf("%s","rrb");
+	return (1);
 }
 
-int ft_rrr(t_stack *a, t_stack *b)
+int	ft_rrr(t_stack *a, t_stack *b)
 {
-    ft_rra(&a);
-    ft_rrb(&b);
-    //ft_printf("%s","rrr");
-    return(1);
+	ft_rra(&a);
+	ft_rrb(&b);
+	// ft_printf("%s","rrr");
+	return (1);
 }
