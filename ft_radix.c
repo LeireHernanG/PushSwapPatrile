3fbff92fbc2673a42259602f17e6ea0076aeb626
Leire <leire.hernang@gmail.com>
2026-02-24 18:22:29 +0100

    Se crea el archivo de el algoritmo de radix comentando las primeras ideas,tambien se añade el prototipo en el .h

---
+++ ft_radix.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_radix.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhernan- <lhernan-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 16:32:57 by lhernan-          #+#    #+#             */
+/*   Updated: 2026/02/24 18:20:51 by lhernan-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "push_swap.h"
+
+int ft_radix(t_stack **a, t_stack **b)
+{
+    int size_a;
+    int index;
+    int i;
+
+    index = 0;
+    i = 0;
+
+
+    //OPCION 1
+    //Seria contar el numero de digitos ddel numero maximo,ej: si tengo 55,1,464, seria 3,y tendriamos que sumarle 1 para saber cuantas veces hariamos el proceso
+    // el stack B seria de 10,del 0-9 pq estan en base decimal,irimaos recorriend el stack A comprobando el ultimo dihito de cada numero y pasandolo al B,ej: el 1 iria al indice 1 del stackB,
+    //el 55 iria al indice 5 y el 464 al 4. Una vez pasado la primera ronda se vuelven a pasar al sstack A en el orden del indice, quedando 4 55 y 464, en la siguiente ronda en cuyo caso que no estuviera ordenado
+    // se pasaria a pasarlo en el stack B comparando con el penultimo digito y asi hasta que estuvieran todos ordenados
+
+    //OPCION 2
+    //pasar al indice, si tengo 3 2 1, el indice seria el 1->0, el 2->1 y el 3->2. de ahi miro lo sbites maximos para saber cuantas veces itero
+    // luego con el bitmax >> 1 voy avanzando y comparando con el &1 == 0 lo muevo a stack b,entonces luego miro l siguiente bit y quedara todo como 000,001,010...
+    size_a = ft_lstsize_st(*a);
+    while(size_a > i)
+    {
+        if((*a)->content == 0)
+            ft_pb(a, b);
+        else
+            ft_ra(a);
+        i++;
+    }
+    return (1);
+}
