701da510603db83db63d68043b5e6c0a00547984
pmieres- <pmieres-@student.42madrid.com>
2026-02-24 16:43:54 +0100

    main para probar inputs y algoritmos

---
+++ main_prove_inputs.c
@@ -0,0 +1,37 @@
+#include "push_swap.h"
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	int algorithm;
+	t_stack *stackA;
+	t_stack *stackB;
+
+	if (argc <= 1)
+	{
+		printf("Pocos argumentos");
+		return (1);
+	}
+	algorithm = control_errors(argc, argv);
+	if (algorithm == 0)
+	{
+		printf("inputs incorrectos");
+		return (1);
+	}
+	printf("%d\n", algorithm);
+	stackA = fill_stack(argc, argv);
+	stackB = stackA;
+	while (stackB)
+	{
+		printf("%d\n", stackB->content);
+		stackB = stackB->next;
+	}
+	ft_buble_sort(&stackA);
+	stackB = stackA;
+	while (stackB)
+	{
+		printf("%d\n", stackB->content);
+		stackB = stackB->next;
+	}
+	return (0);
+}
